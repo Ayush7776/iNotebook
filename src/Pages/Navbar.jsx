4c022b822c40e439e0d36374549ce3fc11968e92
Ayush7776 <ayushpathak7776@gmail.com>
2024-11-22 14:45:24 +0530

    Profile Photo Css Fixed

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,51 +1,35 @@
 import React, { useContext } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import AuthContext from '../Context/AuthContex'
 
 function Navbar() {
-    let location = useLocation()
-
     let { user, logoutUser } = useContext(AuthContext)
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">iNoteBook</Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <Link className="navbar-brand" to="/"><span className='fw-normal'>{user && user.name}'s</span> iNoteBook</Link>
+                    <Link to="/" className="d-flex d-lg-none border border-3 rounded-circle" data-bs-toggle="modal" data-bs-target="#exampleModal3" aria-expanded="true">
+                        <img src={`https://inotebook-backend-6cei.onrender.com${user.profile_pic}`} alt="mdo" width="32" height="32" className="rounded-circle" />
+                    </Link>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        {
-                            location.pathname === "/"
-                                ?
-                                (<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-    
-                                </ul>)
-                                :
-                                (<ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>)
 
-                        }
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
                         <div className="d-flex" role="search">
-                            <h4 className='mx-3'>Hello {user && user.name}</h4>
-
-                            <div className="flex-shrink-0 dropdown">
+                            <div className="d-none d-lg-flex border border-3 rounded-circle">
                                 <Link to="/" className="d-block link-body-emphasis text-decoration-none" data-bs-toggle="modal" data-bs-target="#exampleModal3" aria-expanded="true">
                                     <img src={`https://inotebook-backend-6cei.onrender.com${user.profile_pic}`} alt="mdo" width="32" height="32" className="rounded-circle" />
                                 </Link>
-
-
                                 <div className="modal fade" id="exampleModal3" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                     <div className="modal-dialog">
-                                        <div className="modal-content" style={{ backgroundColor: "transparent", border: "none"}}>
+                                        <div className="modal-content" style={{ backgroundColor: "transparent", border: "none" }}>
                                             <div className="modal-body m-auto">
                                                 <div className="card">
                                                     <button type="button" className="btn-close position-absolute top-0 end-0 me-3" data-bs-dismiss="modal" aria-label="Close"></button>
 
 
                                                     <div className="top-container ">
-
                                                         <img src={`https://inotebook-backend-6cei.onrender.com${user.profile_pic}`} alt='profile' className="img-fluid profile-image rounded-5 shadow-lg" width="150" />
-
                                                     </div>
 
                                                     <div className="ml-3 text-center">
